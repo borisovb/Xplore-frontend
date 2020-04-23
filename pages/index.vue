b2e75cd331ab9fb475e932e858813b5998474c94
Borisov,Boris <boris.borisov@wolfpackit.nl>
2020-04-23 11:43:36 +0200

    [20] Initial layout (index page)

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,99 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Nuxt Documentation
-        </a>
-        <a
-          href="https://github.com/borisovb/Xplore-frontend"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="w-full">
+    <div class="bg-gray-600 w-full h-16">
+      <!-- Navbar -->
+    </div>
+    <div class="flex mb-4 my-10">
+      <div class="w-2/3">
+        <div class="mx-16">
+          <div class="text-5xl font-normal">Xplore your gaming interests</div>
+          <span class="text-3xl font-light"
+            >Find amazing games avaiable on
+          </span>
+          <transition name="slide-fade" mode="out-in">
+            <span :key="textValue" class="text-3xl font-bold">
+              {{ textValue }}</span
+            >
+          </transition>
+        </div>
       </div>
+      <div class="w-1/3">
+        <div class="w-1/2">
+          <img class="p-1" src="~static/gamepad.png" />
+        </div>
+      </div>
+    </div>
+
+    <div class="text-3xl font-normal mx-10">New & Trending</div>
+    <div class="h-56 grid grid-cols-4 gap-4 mx-10 my-5">
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+    </div>
+    <div class="text-3xl font-normal mx-10">Recommendations</div>
+    <div class="h-56 grid grid-cols-4 gap-4 mx-10 my-5">
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+    </div>
+    <div class="text-3xl font-normal mx-10">Best deals</div>
+    <div class="h-56 grid grid-cols-4 gap-4 mx-10 my-5">
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
+      <div class="w-full h-full bg-gray-600"></div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import _ from 'lodash'
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      textValues: [
+        'Xbox',
+        'Playstation',
+        'PC',
+        'Mac',
+        'Linux',
+        'Steam',
+        'Epic Games Store',
+        'GOG.com'
+      ],
+      textValue: null,
+      timeoutText: null
+    }
+  },
+  mounted() {
+    this.changeText()
+  },
+  destroyed() {
+    this.timeoutText = null
+  },
+  methods: {
+    changeText() {
+      this.textValue = _.sample(this.textValues)
+      this.timeoutText = setTimeout(this.changeText, 3500)
+    }
   }
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
+.slide-fade-leave-active {
+  transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.links {
-  padding-top: 15px;
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
