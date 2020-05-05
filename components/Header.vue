72b8fc4f8387a18aeb8b334c5f5b036e32e1379d
Perkingsian <parham_377i@yahoo.com>
2020-05-05 22:53:57 +0200

    added sign up

--- components/Header.vue
+++ components/Header.vue
@@ -32,20 +32,27 @@
           href="#responsive-header"
           class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
         >
-          Docs
+          News
         </a>
         <a
           href="#responsive-header"
           class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
         >
-          Examples
+          Games
         </a>
         <a
           href="#responsive-header"
           class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
         >
-          Blog
+          Platforms
         </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="rounded-full inline-block text-sm px-4 py-2 mr-3 leading-none border rounded text-white border-teal hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+          >Sign Up</a
+        >
       </div>
       <div>
         <a
